ce1593ae631dacde3fe4eb533df13703b3246871
mieshiii <mieseliel.delacruz@gmail.com>
2023-10-04 23:41:34 +0800

    #2 - add test etl files

---
+++ dev_etl/etl.py
@@ -0,0 +1,16 @@
+# import pyspark
+from pyspark.sql.functions import col
+from pyspark.sql.types import IntegerType, DoubleType, BooleanType, DateType
+
+# config?
+configs = {"fs.azure.account.auth.type": "OAuth",
+"fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+"fs.azure.account.oauth2.client.id": "",
+"fs.azure.account.oauth2.client.secret": '',
+"fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/tanent_id/oauth2/token"}
+
+
+dbutils.fs.mount(
+source = "abfss://tokyo-olympic-data@tokyoolympicdata.dfs.core.windows.net", # contrainer@storageacc
+mount_point = "/mnt/tokyoolymic",
+extra_configs = configs)
